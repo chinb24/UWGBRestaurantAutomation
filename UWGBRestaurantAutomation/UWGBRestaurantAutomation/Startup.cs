5c389135fa2d24ca302dc6a02e64d16d578457ad
Bobby Chindaphone <Bobby Chindaphone@BCHINDAPH-7SFS>
2017-03-21 10:31:36 -0500

    Base subversion

---
+++ UWGBRestaurantAutomation/UWGBRestaurantAutomation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UWGBRestaurantAutomation.Startup))]
+namespace UWGBRestaurantAutomation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
