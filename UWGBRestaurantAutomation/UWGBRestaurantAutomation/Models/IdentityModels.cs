efde18c67f9a581a768de4004250dbd9a14445fd
chinb24@uwgb.edu <chinb24@uwgb.edu>
2017-04-10 12:20:19 -0500

    Register and log in should now work

--- UWGBRestaurantAutomation/UWGBRestaurantAutomation/Models/IdentityModels.cs
+++ UWGBRestaurantAutomation/UWGBRestaurantAutomation/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace UWGBRestaurantAutomation.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("RestaurantContext", throwIfV1Schema: false)
         {
         }
 
