c1ba2e98840b64115918bcffea908e05123a7823
chinb24@uwgb.edu <chinb24@uwgb.edu>
2017-05-01 08:55:31 -0500

    Various minor changes

---
+++ UWGBRestaurantAutomation/UWGBRestaurantAutomation/Models/ServerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UWGBRestaurantAutomation.Models
+{
+    public class ServerViewModel
+    {
+        public int OrderId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductImage { get; set; }
+        public bool Ordering { get; set; }
+        public int TableNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
